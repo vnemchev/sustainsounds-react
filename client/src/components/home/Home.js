ba586d72fbb235e372bfba1f66626e5db0e43d76
vnemchev <vnemchev@gmail.com>
2022-08-12 23:17:33 +0200

    Homepage looking good.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,37 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 
 const Home = () => {
     return (
         <>
-            <div className={styles.container}>
-                <h1>Welcome to sustainsounds!</h1>
-                <p>
-                    We pride ourselves in creating a fair and modern service for
-                    musicians and performers worldwide.
-                </p>
+            <div className={styles.homeContainer}>
+                <div className={styles.intro}>
+                    <h1 className={styles.homeHeading}>
+                        Welcome to sustainsounds!
+                    </h1>
+                    <h3 className={styles.homeSlogan}>
+                        We pride ourselves in creating a fair and modern service
+                        for musicians and performers worldwide.
+                    </h3>
+                    <p className={styles.homeText}>
+                        Since 2022 we have been giving creators and fans a
+                        platform to explore, create, and attend different
+                        musical events.
+                        {'\n'}
+                        You too can be a part of our family. All it takes is a
+                        quick sign up. Happy exploring!
+                    </p>
+                </div>
+
+                <div className={styles.links}>
+                    <Link className={styles.link} to={'/events'}>
+                        events
+                    </Link>
+                    <Link className={styles.link} to={'/artists'}>
+                        artists
+                    </Link>
+                </div>
             </div>
         </>
     );
